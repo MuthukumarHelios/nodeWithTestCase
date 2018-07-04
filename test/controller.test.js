d01b8a69d8d88c79b4b339dff4f2f03a970c1354
kumar001 <muthukumar.s@transneuron.com>
2018-07-05 00:48:16 +0530

    test case for await

--- test/controller.test.js
+++ test/controller.test.js
@@ -1,10 +1,22 @@
 
 
 var controller = require("../controlers/controler")
+var sinon = require('sinon');
 
+var chai = require('chai');
+var expect = chai.expect;
+
+var req,res, spy;
+req = res = {};
+spy = res.json = sinon.spy();
 
 describe("findBatches",  async () => {
+  it("should return response", async() => {
+   await controller.findBatches(req, res)
+    expect(spy.calledOnce).to.equal(true)
+  })
   it("should return err", async() => {
-//    batches = await controller.findBatches()
-  })    
+    // expect(spy.).to.equal(true)
+      
+  })
 })
